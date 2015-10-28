001c3ed9eb3e6b14481cba221e478c3f52e1442b
Nephna <devin.westbye@uoit.net>
2015-10-28 08:39:23 -0400

    Added ability to read in puzzle and store in array

--- sudoku.c
+++ sudoku.c
@@ -1,8 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#define BUFFER_LEN 256
+#define PUZZLETXT "puzzle.txt"
 
-int main(void)
-{
+void readInPuzzle (char puzzle[9][9]) {
+	FILE *file = fopen(PUZZLETXT, "r"); // open puzzle.txt for reading
+	if (file != NULL) {
+		char line[19]; // length 19 in order to hold 9 chars, 9 spaces, and 1 newline character
 
+		for (int i = 0; i < 9; i++) { // loop for each row of the puzzle
+			int lineSize = (sizeof(line) / sizeof(line[0])); // get lineSize
+
+			if(fgets(line, lineSize, file) != NULL) {
+				 // loop for every-other value in the line, starting from 0
+				for (int j = 0; j < lineSize-2; j+=2) { // use lineSize-2 to eliminate newline char
+					puzzle[i][j/2] = line[j];
+				}
+			}
+		}
+	}
+	fclose(file);
+}
+
+int main (void) {
+	// variable declaration
+	char puzzle[9][9];
+
+	readInPuzzle(puzzle);
 }
